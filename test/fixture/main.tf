31eca02a3528089dcca8a8aa772903e4e452a6fe
Massimiliano Giovagnoli <massimiliano.giovagnoli.1992@gmail.com>
2020-08-18 17:53:03 +0200

    fix(test/fixture/main): add required subnet reference for the nodepool

--- test/fixture/main.tf
+++ test/fixture/main.tf
@@ -10,10 +10,16 @@ resource "azurerm_resource_group" "main" {
   location = var.location
 }
 
+module network {
+  source              = "Azure/network/azurerm"
+  resource_group_name = azurerm_resource_group.main.name
+}
+
 module aks {
   source              = "../.."
   prefix              = "prefix-${random_id.prefix.hex}"
   resource_group_name = azurerm_resource_group.main.name
+  subnet_id           = module.network.vnet_subnets[0]
 }
 
 module aks_without_monitor {
@@ -21,4 +27,5 @@ module aks_without_monitor {
   prefix                         = "prefix2-${random_id.prefix.hex}"
   resource_group_name            = azurerm_resource_group.main.name
   enable_log_analytics_workspace = false
+  subnet_id                      = module.network.vnet_subnets[0]
 }
