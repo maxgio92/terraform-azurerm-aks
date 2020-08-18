eadb2784a3acecb0fd0807caa7488b82ecb831e4
Massimiliano Giovagnoli <massimiliano.giovagnoli.1992@gmail.com>
2020-08-18 15:26:42 +0200

    fix(modules/ssh-key): create the private key file with proper modes

--- modules/ssh-key/main.tf
+++ modules/ssh-key/main.tf
@@ -4,9 +4,10 @@ resource "tls_private_key" "ssh" {
 }
 
 resource "local_file" "private_key" {
-  count    = var.public_ssh_key == "" ? 1 : 0
-  content  = tls_private_key.ssh.private_key_pem
-  filename = "./private_ssh_key"
+  count             = var.public_ssh_key == "" ? 1 : 0
+  sensitive_content = tls_private_key.ssh.private_key_pem
+  filename          = "./private_ssh_key"
+  file_permission   = "0400"
 }
 
 output "public_ssh_key" {
